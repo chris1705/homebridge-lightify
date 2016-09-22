d4088b4a2e272176be2055bca9b81f1d7df42185
Christian Fender <chris@Christians-MBP.fritz.box>
2016-09-22 17:12:51 +0200

    Now it's the really working version..

--- index.js
+++ index.js
@@ -1,115 +1,88 @@
 var lightify = require('node-lightify'),
            _ = require('underscore');
 
-var Service, Characteristic;
+var Accessory, Service, Characteristic, UUIDGen;
 
+module.exports = function(homebridge) {
+ Accessory = homebridge.platformAccessory;
+ Service = homebridge.hap.Service;
+ Characteristic = homebridge.hap.Characteristic;
+ UUIDGen = homebridge.hap.uuid;
+ homebridge.registerPlatform("homebridge-lightify", "Lightify", LightifyPlatform);
+}
 
-function LightifyPlatform(log, config, api) {
-   console.log("HELLO! ITS ME! LGIHTFY!!");
-   log("HI");
-   this.log = log;
-   this.config = config;
-   this.host = config['host']
-   this.api = api;
-   this.accessories = []
-   try {
-     this.init();
-   } catch (e) {
-     this.log(e);
-   }
- }
+class LightifyPlatform {
 
- LightifyPlatform.prototype.init = function(callback) {
-   this.log("hello");
-   let self = this;
-   const platformAccessory = new Accessory(name, UUIDGen.generate(self.host), 5 /* Accessory.Categories.LIGHTBULB_TCTYPE */);
-
-   lightify.start(host).then(function(data){
-       return lightify.discovery();
-   }).then(function(response) {
-       self.log(response);
-       let list = _.map(response.result, (device) => {
-         // We will only add lights
-         //if(lightify.isLight(device['type'])) {
-         self.log(device);
-           self.addAccessory(device);
-         //}
-       });
-       //callback(list);
-       //this.api.registerPlatformAccessories('homebridge-lightify', 'Lightify', [platformAccessory]);
-   });
- }
-
- LightifyPlatform.prototype.addAccessory = function(device) {
-   this.log("Registration of:" , device);
-   let self = this;
-   let uuid = UUIDGen.generate(device.name);
-   let accessory = new Accessory(device.name, uuid);
-   accessory.addService(Service.Lightbulb, device.name)
-   .getCharacteristic(Characteristic.On)
-   .on('set', function(value, callback) {
-     self.lightify.node_on_off(device.mac, value);
-    callback();
-    });
-    this.accessories.push(newAccessory);
-    this.api.registerPlatformAccessories("homebridge-lightify", "Lightify", [accessory]);
- }
-
-
-class LightifyAccessory {
-
-  constructor(log, config, lightify, device) {
+  constructor(log, config, api) {
     this.log = log;
     this.config = config;
-    this.id = device['id'];
-    this.name = device['name'];
-    this.fw_version = device['firmware_version'];
-    this.device = device;
-    this.lightify = lightify;
     this.api = api;
-    let self = this;
-    this.api.registerPlatformAccessories("homebridge-lightify", "Lightify", self.getServices());
+    this.host = config["host"];
   }
 
-  setOn(state, callback) {
+  accessories(callback) {
     let self = this;
-    self.lightify.node_on_off(self.device['mac'], state);
-    if(callback) {
-      callback();
-    }
+    lightify.start(this.host).then((data) => {
+      return lightify.discovery();
+    }).then((data) => {
+      let accessories = _.map(data.result, (device) => {
+        return new LightifyLamp(device.name, UUIDGen.generate(device.name), device.mac, this.lightify);
+      });
+      callback(accessories);
+    });
+  }
+}
+
+class LightifyLamp {
+
+  constructor(name, uuid, mac, lighitfy) {
+    this.name = name;
+    this.uuid = uuid;
+    this.lightify = lightify;
+    this.mac = mac;
   }
 
-  setOff(callback) {
-    let self = this;
-    this.lightify.discovery().then((response) => {
-      let info = _.findWhere(response.result, {"id": self.id});
-      callback(status == 1);
+  setState(value, callback) {
+    lightify.node_on_off(this.mac, value);
+    callback();
+  }
+
+  setBrightness(value, callback) {
+    lightify.node_brightness(this.mac, value);
+    callback();
+  }
+
+  getState(callback) {
+    var self = this;
+    lightify.discovery().then((data) => {
+      let device = _.findWhere(data.result, {"mac": self.mac});
+      callback(null, device.status === 1 || device.online === 1);
+    });
+  }
+
+  getBrightness(callback) {
+    var self = this;
+    lightify.discovery().then((data) => {
+      let device = _.findWhere(data.result, {"mac": self.mac});
+      callback(null, device.brightness);
     });
   }
 
   getServices() {
     let self = this;
-    let informationService = new Service.AccessoryInformation();
-    let lightbulbService = new Service.Lightbulb(this.name);
+    var service = new Service.AccessoryInformation();
+    service.setCharacteristic(Characteristic.Name, this.name)
+           .setCharacteristic(Characteristic.Manufacturer, "OSRAM Licht AG")
+           .setCharacteristic(Characteristic.Model, "Lightify");
 
-	  informationService.setCharacteristic(Characteristic.Manufacturer, "OSRAM Licht AG")
-                  		.setCharacteristic(Characteristic.Model, "Lightify")
-                  		.addCharacteristic(Characteristic.FirmwareRevision, this.fw_version);
+    var lightService = new Service.Lightbulb(this.name);
+    lightService.getCharacteristic(Characteristic.On)
+                .on('set', (a, b) => {self.setState(a, b); })
+                .on('get', (a, b) => {self.getState(a, b); });
+    lightService.getCharacteristic(Characteristic.Brightness)
+                .on('set', (a, b) => {self.setBrightness(a, b); })
+                .on('get', (a, b) => {self.getBrightness(a, b); });
 
-    lightbulbService.getCharacteristic(Characteristic.On)
-                    .on('get', self.getOn)
-                    .on('set', self.setOn)
-
-    return [informationService, lightbulbService];
+    return [service, lightService];
   }
-
-}
-
-module.exports = function(homebridge) {
-  console.log("homebridge API version: " + homebridge.version);
-
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-
-  homebridge.registerPlatform("homebridge-lightify", "Lightify", LightifyPlatform, true);
 }
